36819caf7b3eb7ca530ea4f82151526bbaf3780b
Melissa Linkert <melissa@glencoesoftware.com>
2019-10-06 13:29:24 -0500

    Add method to write metadata to a JSON file

--- write_tiles.py
+++ write_tiles.py
@@ -8,6 +8,7 @@
 # missing please request a copy by contacting info@glencoesoftware.com
 
 import argparse
+import json
 import os
 from concurrent import futures
 from math import ceil
@@ -31,6 +32,7 @@ def main():
     no_pyramid = args.no_pyramid
 
     slide_directory, pe_in, pixel_engine = setup(input_file)
+    write_metadata(slide_directory, pe_in)
     write_label_image(slide_directory, pe_in)
     write_macro_image(slide_directory, pe_in)
     write_pyramid(slide_directory, pe_in, pixel_engine, tile_width, tile_height, no_pyramid)
@@ -48,6 +50,53 @@ def setup(input_file):
     pixel_engine = pixelengine.PixelEngine(render_backend, render_context)
     pixel_engine["in"].open(input_file, "ficom")
     return slide_dir, pixel_engine["in"], pixel_engine
+
+# write metadata to a JSON file
+def write_metadata(slide_directory, pixel_engine):
+    metadata_file = open(slide_directory + os.sep + "METADATA.json", "wb")
+
+    metadata = dict()
+    metadata["Barcode"] = pixel_engine.BARCODE
+    metadata["DICOM acquisition date"] = pixel_engine.DICOM_ACQUISITION_DATETIME
+    metadata["DICOM last calibration date"] = pixel_engine.DICOM_DATE_OF_LAST_CALIBRATION
+    metadata["DICOM time of last calibration"] = pixel_engine.DICOM_TIME_OF_LAST_CALIBRATION
+    metadata["DICOM manufacturer"] = pixel_engine.DICOM_MANUFACTURER
+    metadata["DICOM manufacturer model name"] = pixel_engine.DICOM_MANUFACTURERS_MODEL_NAME
+    metadata["DICOM device serial number"] = pixel_engine.DICOM_DEVICE_SERIAL_NUMBER
+    metadata["Color space transform"] = pixel_engine.colorspaceTransform()
+    metadata["Block size"] = pixel_engine.blockSize()
+    metadata["Number of tiles"] = pixel_engine.numTiles()
+    metadata["Bits stored"] = pixel_engine.bitsStored()
+    metadata["Derivation description"] = pixel_engine.DICOM_DERIVATION_DESCRIPTION
+    metadata["DICOM software version"]= pixel_engine.DICOM_SOFTWARE_VERSIONS
+    metadata["Number of images"] = pixel_engine.numImages()
+
+    for image in range(pixel_engine.numImages()):
+        image_metadata = dict()
+        image_metadata["Image type"] = pixel_engine[image].IMAGE_TYPE
+        image_metadata["DICOM lossy image compression method"] = pixel_engine[image].DICOM_LOSSY_IMAGE_COMPRESSION_METHOD
+        image_metadata["DICOM lossy image compression ratio"] = pixel_engine[image].DICOM_LOSSY_IMAGE_COMPRESSION_RATIO
+        image_metadata["DICOM derivation description"] = pixel_engine[image].DICOM_DERIVATION_DESCRIPTION
+        image_metadata["Image dimension names"] = pixel_engine[image].IMAGE_DIMENSION_NAMES
+        image_metadata["Image dimension types"] = pixel_engine[image].IMAGE_DIMENSION_TYPES
+        image_metadata["Image dimension units"] = pixel_engine[image].IMAGE_DIMENSION_UNITS
+        image_metadata["Image dimension ranges"] = pixel_engine[image].IMAGE_DIMENSION_RANGES
+        image_metadata["Image dimension discrete values"] = pixel_engine[image].IMAGE_DIMENSION_DISCRETE_VALUES_STRING
+        image_metadata["Image scale factor"] = pixel_engine[image].IMAGE_SCALE_FACTOR
+
+        if pixel_engine[image].IMAGE_TYPE == "WSI":
+            view = pixel_engine.SourceView()
+            image_metadata["Bits allocated"] = view.bitsAllocated()
+            image_metadata["Bits stored"] = view.bitsStored()
+            image_metadata["High bit"] = view.highBit()
+            image_metadata["Pixel representation"] = view.pixelRepresentation()
+            image_metadata["Planar configuration"] = view.planarConfiguration()
+            image_metadata["Samples per pixel"] = view.samplesPerPixel()
+            image_metadata["Number of levels"] = pixel_engine.numLevels()
+
+        metadata["Image #" + str(image)] = image_metadata
+
+    json.dump(metadata, metadata_file)
 
 # write the label image (if present) as a JPEG file
 def write_label_image(slide_directory, pixel_engine):
