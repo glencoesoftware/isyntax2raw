aea6dc32cc984b6ed3381fab55bef123543cd676
Chris Allan <callan@glencoesoftware.com>
2019-10-25 12:58:02 +0100

    Make sure that the writing task queue is not unbounded
    This commit adds a bounded queue/pool which via delegation strictly
    limits the number of in flight tasks.
    
    In most cases on modern hardware with high frequency and multi-core CPUs
    it will be faster to read and decompress data from the source than to
    write it out to its destination.  This is true regardless of destination
    file format or the presence of high performance storage.
    
    A `ThreadPoolExecutor` in its standard form maintains an unbounded queue
    for tasks.  Thus, `ThreadPoolExecutor.submit()` will never block and it
    will happily take new tasks until system memory is exhausted.  That's
    **very** bad for our use case as it rather quickly results in a very
    large number tasks operating on 1.5MiB or more of data being
    accumulated.  GiBs of system memory is then consumed.

--- isyntax2raw/__init__.py
+++ isyntax2raw/__init__.py
@@ -16,11 +16,53 @@ import psutil
 import softwarerendercontext
 import softwarerenderbackend
 
-from concurrent import futures
+from concurrent.futures import ALL_COMPLETED, ThreadPoolExecutor, wait
 from math import ceil
+from threading import BoundedSemaphore
 
 from PIL import Image
 from tifffile import imwrite
+
+
+class MaxQueuePool(object):
+    """This Class wraps a concurrent.futures.Executor
+    limiting the size of its task queue.
+    If `max_queue_size` tasks are submitted, the next call to submit will
+    block until a previously submitted one is completed.
+
+    Brought in from:
+      * https://gist.github.com/noxdafox/4150eff0059ea43f6adbdd66e5d5e87e
+
+    See also:
+      * https://www.bettercodebytes.com/
+            theadpoolexecutor-with-a-bounded-queue-in-python/
+      * https://pypi.org/project/bounded-pool-executor/
+      * https://bugs.python.org/issue14119
+      * https://bugs.python.org/issue29595
+      * https://github.com/python/cpython/pull/143
+    """
+    def __init__(self, executor, max_queue_size, max_workers=None):
+        self.pool = executor(max_workers=max_workers)
+        self.pool_queue = BoundedSemaphore(max_queue_size)
+
+    def submit(self, function, *args, **kwargs):
+        """Submits a new task to the pool, blocks if Pool queue is full."""
+        self.pool_queue.acquire()
+
+        future = self.pool.submit(function, *args, **kwargs)
+        future.add_done_callback(self.pool_queue_callback)
+
+        return future
+
+    def pool_queue_callback(self, _):
+        """Called once task is done, releases one queue slot."""
+        self.pool_queue.release()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.pool.__exit__(exception_type, exception_value, traceback)
 
 class WriteTiles(object):
 
@@ -234,9 +276,9 @@ class WriteTiles(object):
                         source.save(destination)
 
             jobs = ()
-            with futures.ThreadPoolExecutor(
-                max_workers=psutil.cpu_count(logical=False)
-            ) as executor:
+            with MaxQueuePool(
+                ThreadPoolExecutor, psutil.cpu_count(logical=False)
+            ) as pool:
                 while regions:
                     regions_ready = self.pixel_engine.waitAny(regions)
                     for region_index, region in enumerate(regions_ready):
@@ -256,10 +298,10 @@ class WriteTiles(object):
                         filename = os.path.join(
                             directory, "%s.%s" % (y_start, self.file_type)
                         )
-                        jobs = jobs + (executor.submit(
+                        jobs = jobs + (pool.submit(
                             write_tile, pixels, width, height, filename
                         ),)
-            futures.wait(jobs, return_when=futures.ALL_COMPLETED)
+            wait(jobs, return_when=ALL_COMPLETED)
 
     def create_patch_list(
         self, tiles, tile_size, origin, level, tile_directory
