0c6678a88ff3cc7f5abac56af5f9c41ad45ae04a
Chris Allan <callan@glencoesoftware.com>
2020-01-06 11:12:07 +0000

    Reorder imports to be more in line with Pythonic convention

--- isyntax2raw/__init__.py
+++ isyntax2raw/__init__.py
@@ -7,22 +7,22 @@
 # file you can find at the root of the distribution bundle.  If the file is
 # missing please request a copy by contacting info@glencoesoftware.com
 
-from datetime import datetime
 import json
 import math
 import os
 
-from kajiki import PackageLoader
 import numpy as np
 import pixelengine
 import softwarerendercontext
 import softwarerenderbackend
 import zarr
 
+from datetime import datetime
 from concurrent.futures import ALL_COMPLETED, ThreadPoolExecutor, wait
 from threading import BoundedSemaphore
 
 from PIL import Image
+from kajiki import PackageLoader
 from tifffile import imwrite
 
 
